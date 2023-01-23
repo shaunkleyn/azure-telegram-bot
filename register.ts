30401943eed68c0550080e3175b3332400ea4df8
Shaun <shaunkleyn@gmail.com>
2023-01-23 19:27:01 +0200

    removed files

--- register.ts
+++
@@ -1,9 +0,0 @@
-import {Telegraf} from 'telegraf';
-const token = process.env["TELEGRAM_BOT_TOKEN"];
-const url = process.env["WEBHOOK_ADDRESS"]
-if (!token || !url) {
-    console.log("Missing TELEGRAM_BOT_TOKEN and WEBHOOK_ADDRESS environment variables")
-} else {
-    const bot = new Telegraf(token);
-    bot.telegram.setWebhook(url);
-}
